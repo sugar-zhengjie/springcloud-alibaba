1d4039609c23882bf9fdeb00c4cb32bbbd63f205
sugar_zhengjie <2421275302@qq.com>
2023-11-09 15:37:53 +0800

    gateway first commit

---
+++ Commons/src/main/java/com/zj/constant/BaseConstants.java
@@ -0,0 +1,32 @@
+package com.zj.constant;
+
+/**
+ * @author zhengjie
+ * @version 1.0
+ * @date 2023/11/9 14:25
+ */
+public class BaseConstants {
+
+    public static final String DEFAULT_CHARSET_NAME = "UTF-8";
+
+    public static final String HTTP_HEADER_AUTH_CONTEXT = "X-AUTH-CONTEXT";
+
+    public static final String HTTP_HEADER_GATEWAY_SIGN = "X-GATEWAY-SIGN";
+    public static final String HTTP_HEADER_GATEWAY_SIGN_VALUE = "GATEWAY-SIGN";
+
+    public static final String HTTP_HEADER_USER_ID = "X-AUTH-USER-ID";
+    public static final String HTTP_HEADER_USER_NAME = "X-AUTH-USER-NAME";
+
+    public static final String HTTP_HEADER_FEIGN_SIGN = "X-FEIGN-SIGN";
+    public static final String HTTP_HEADER_FEIGN_SIGN_VALUE = "FEIGN-SIGN";
+
+    public static final Integer MAX_PAGE_SIZE = 2000;
+    public static final Integer DEFAULT_PAGE_SIZE = 20;
+
+
+    public static final String ACCESS_TOKEN = "ACCESS-TOKEN";
+
+    public static final String EXCEPTION_ID = "EXCEPTION-ID";
+
+
+}

ab31ae834bcbe1f079a86f63f9388067119ca34b
sugar_zhengjie <2421275302@qq.com>
2023-11-10 14:31:59 +0800

    mybatis plus 自定义方法

--- User/src/main/java/com/zj/dao/UserMapper.java
+++ User/src/main/java/com/zj/dao/UserMapper.java
@@ -1,7 +1,14 @@
 package com.zj.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
 import com.zj.entity.User;
+import org.apache.ibatis.annotations.Param;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
+import java.io.Serializable;
+import java.util.Collection;
 
 /**
  * @author zhengjie
@@ -9,4 +16,13 @@ import com.zj.entity.User;
  * @date 2023/11/10 14:10
  */
 public interface UserMapper extends BaseMapper<User> {
+
+    int insertOne(User user);
+
+    int updateUserById(@Param("id") User user);
+
+    IPage<User> selectUserInfoByPage(Page<Object> objectPage,
+                                     @Param("signCondition") String signCondition);
+
+    int deleteUsersByBatchIds(@Param(Constants.COLLECTION) Collection<? extends Serializable> idList);
 }
